738e14d1cca5ee88743e13c184a45f9dc5667d6c
shltn <kawanaka.shltn.shogo.ubi@gmail.com>
2017-09-06 15:38:31 +0900

    test up

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require("gulp");
+var fs = require("fs");
+var ejs = require("gulp-ejs");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var browser = require("browser-sync");
+var plumber = require("gulp-plumber");
+
+gulp.task("server", function() {
+    browser({
+        server: {
+            baseDir: "./docs",
+            startPath: '/4919/',
+            routes : {
+                '/4919': 'docs'
+            }
+        }
+    });
+});
+
+gulp.task("ejs", function() {
+    var json = JSON.parse(fs.readFileSync("./source/html/variables.json"));
+    gulp.src(["./source/html/**/*.ejs", "!./source/html/template/*.ejs"], { base: "./source/html" })
+        .pipe(plumber())
+        .pipe(ejs(json, {"ext": ".html"}))
+        .pipe(gulp.dest("./docs"))
+        .pipe(browser.reload({stream:true}));
+});
+ 
+gulp.task("sass", function() {
+    gulp.src("./source/sass/**/*scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest("./docs/css"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("js", function() {
+    gulp.src(["./source/js/**/*.js"])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(gulp.dest("./docs/js"))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("reload", function() {
+    browser.reload();
+});
+
+gulp.task("default", ["server"], function() {
+    gulp.watch("./source/html/**/*.ejs", ["ejs"]);
+    gulp.watch("./source/js/**/*.js",["js"]);
+    gulp.watch("./source/sass/**/*.scss",["sass"]);
+});
